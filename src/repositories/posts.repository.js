384eb3c49b1db81c735b9a413cbabf2c681d8123
totobon <totobon6125@gmail.com>
2023-09-17 01:14:44 +0900

    repository

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -1,1 +1,18 @@
-export class PostsRepository {}
+import {prisma} from '../utils/prisma/index.js'
+
+export class PostsRepository {
+    findAllPosts = async () => {
+        const posts = await prisma.posts.findMany();
+
+        return posts;
+    }
+
+    createPost = async (nickname, password, title, content) => {
+        const createdPost = await prisma.posts.create({
+            data: {
+                nickname, password, title, content
+            }
+        });
+        return createdPost;
+    };
+}
